b86935cfe77b6b8db13a9ffe34b1f0356635ce52
ki-ki13 <rizkimahjati845@students.unnes.ac.id>
2021-08-08 22:18:33 +0700

    add db

--- penugasan.sql
+++ penugasan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Agu 2021 pada 19.32
+-- Waktu pembuatan: 08 Agu 2021 pada 17.15
 -- Versi server: 10.4.6-MariaDB
 -- Versi PHP: 7.3.9
 
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `folder` (
   `id` int(3) NOT NULL,
   `jenis` varchar(10) NOT NULL,
-  `nama_folder` varchar(30) NOT NULL,
+  `nama_folder` varchar(50) NOT NULL,
   `folder_id` varchar(50) NOT NULL,
   `deskripsi` varchar(2000) NOT NULL,
   `link_cont` varchar(1000) NOT NULL,
@@ -43,10 +43,10 @@ CREATE TABLE `folder` (
 --
 
 INSERT INTO `folder` (`id`, `jenis`, `nama_folder`, `folder_id`, `deskripsi`, `link_cont`, `keterangan`) VALUES
-(3, 'individu', 'Upload Twibbon', '1_QBqvmcOEQ1aVDgqgQnSmZjBdMRcwpyH', 'Ini adalah tugas upload twibbon', 'https://drive.google.com/file/d/1Rg-DeBQ81LGUR7RnWkSuhJENErRznwl5/preview', ''),
-(4, 'kelompok', 'Tugas Perkenalan Diri', '1ULEpbXJm0ncSGPo7-10__-3A8kT-hJ16', 'Ini adalah tugas perkenalan diri', '', ''),
+(3, 'individu', 'Upload Twibbon (Pra-Acara)', '1_QBqvmcOEQ1aVDgqgQnSmZjBdMRcwpyH', 'Silakan baca keterangan tugas pada kolom keterangan.', 'https://drive.google.com/file/d/1b4_ai7wY6_2D2Mxbhhz-7WSmQxKIyJGQ/preview', ''),
+(4, 'individu', 'Mipa Creative Challenge (Pra-Acara)', '1QDRAiAIlXviTO-wxPp9XMSx3-P-Is6iW', 'Penugasan ini berupa video yang dibuat oleh mahasiswa baru dengan menyanyikan sebuah lagu dan bagian rumpang pada jingle PKKMB FMIPA UNNES 2021 lalu diikuti dengan menirukan gerakan selebrasi dan jingle  sesuai dengan video tutorial yang sudah diperagakan oleh panitia. \r\nKemudian video penugasan MCC tersebut di upload di akun sosial media (instagram) masing-masing mahasiswa baru dengan menggunakan template yang sudah disediakan dan sesuai dengan ketentuan yang sudah ditetapkan.\r\n', 'https://drive.google.com/file/d/1csWf1SY6RdDSJ2XkIkOJ1sG2hnbRSRFv/preview', ''),
 (5, 'kelompok', 'Tugas Mencari Teman', '1Wy3HLrTySbRAjn4o0u8oQ8bs_05LX8Ss', 'Ini adalah tugas mencari teman', '', ''),
-(6, 'individu', 'Upload VIdeo IG', '1QDRAiAIlXviTO-wxPp9XMSx3-P-Is6iW', 'Ini adalah tugas upload video ig', '', '');
+(7, 'individu', 'Post Test Virtual Tour', '1FImuiETVpHQmizHQey7mHwli8mSWkqle', 'Ini adalah tugas post test mengenai virtual tour.', 'https://docs.google.com/forms/d/e/1FAIpQLSffE7XP7Co6nmggaMaFvihz4HuyiKFPcVQiz4A-c4BM9NV4_g/viewform?embedded=true', '');
 
 -- --------------------------------------------------------
 
@@ -57,18 +57,8 @@ INSERT INTO `folder` (`id`, `jenis`, `nama_folder`, `folder_id`, `deskripsi`, `l
 CREATE TABLE `kelompok` (
   `no` int(11) NOT NULL,
   `login_oauth_uid` varchar(100) NOT NULL,
-  `user_id` int(11) NOT NULL,
   `kelompok` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `kelompok`
---
-
-INSERT INTO `kelompok` (`no`, `login_oauth_uid`, `user_id`, `kelompok`) VALUES
-(1, '116119015737919646772', 2, 1),
-(2, '111360405407430135756', 3, 2),
-(3, '115705964469710459926', 4, 3);
 
 -- --------------------------------------------------------
 
@@ -112,9 +102,9 @@ CREATE TABLE `user_login` (
 --
 
 INSERT INTO `user_login` (`user_id`, `login_oauth_uid`, `first_name`, `last_name`, `email_address`, `profile_picture`, `created_at`, `updated_at`) VALUES
-(2, '116119015737919646772', 'PKKMB FMIPA', 'UNNES 2021', 'pkkmbfmipa@mail.unnes.ac.id', 'https://lh3.googleusercontent.com/a/AATXAJzt7rU6oB', '2021-07-23 06:18:31', '2021-08-02 19:05:56'),
+(2, '116119015737919646772', 'PKKMB FMIPA', 'UNNES 2021', 'pkkmbfmipa@mail.unnes.ac.id', 'https://lh3.googleusercontent.com/a/AATXAJzt7rU6oB', '2021-07-23 06:18:31', '2021-08-04 13:25:43'),
 (3, '111360405407430135756', 'Deny', 'Lukman Syarif', 'cakdeny49@students.unnes.ac.id', 'https://lh3.googleusercontent.com/a/AATXAJzDoxObZe', '2021-07-23 06:25:42', '2021-07-27 17:47:04'),
-(4, '115705964469710459926', 'Rizki', 'Mahjati Prie Husna', 'rizkimahjati845@students.unnes.ac.id', 'https://lh3.googleusercontent.com/a/AATXAJzuGXtCnh', '2021-07-29 15:45:20', '2021-08-02 15:44:36');
+(4, '115705964469710459926', 'Rizki', 'Mahjati Prie Husna', 'rizkimahjati845@students.unnes.ac.id', 'https://lh3.googleusercontent.com/a/AATXAJzuGXtCnh', '2021-07-29 15:45:20', '2021-08-06 13:52:38');
 
 --
 -- Indexes for dumped tables
@@ -152,7 +142,7 @@ ALTER TABLE `user_login`
 -- AUTO_INCREMENT untuk tabel `folder`
 --
 ALTER TABLE `folder`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `kelompok`
